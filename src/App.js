3b0fbff19634c84f58f2b873500210a65700353b
Harvey-Boonsdale <111384710+Harvey-Boonsdale@users.noreply.github.com>
2023-02-22 19:16:16 +0000

    stack set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
 import "./App.css";
+// import { Routes, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import { ApiClient } from "./apiClient";
+// import Login from "./Login";
+// import MyNav from "./Navbar";
+// import InputForm from "./InputForm";
+// import EditForm from "./EditForm";
+
+// initialise properties of opportunity card
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const [opportunities, changeOpportunities] = useState([]);
+  const client = new ApiClient();
+
+  // error message
+
+  const checkStatus = (response) => {
+    if (response.status >= 200 && response.status < 300) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // Gets info from server and puts it in state
+
+  const listOpportunities = () => {
+    client.listOpportunities().then((response) => {
+      let success = checkStatus(response);
+      if (!success) {
+        alert("Error connecting - please try again");
+        return;
+      }
+      changeOpportunities(response.data);
+    });
+  };
+
+  return (
+    <Container>
+      <Dashboard
+        listOpportunities={listOpportunities}
+        opportunities={opportunities}
+        client={client}
+      />
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import "./App.css";
+import OpportunityCard from "./OpportunityCard";
+
+function Dashboard(props) {
+  useEffect(() => {
+    props.listOpportunities();
+  }, []);
+
+  const makeOpportunities = () => {
+    return props.opportunities.map((opportunity) => {
+      return (
+        <OpportunityCard
+          postToDisplay={opportunity}
+          client={props.client}
+          listEvents={props.listOpportunities}
+        />
+      );
+    });
+  };
+
+  //print opportunity card
+
+  return (
+    <div>
+      <Container>
+        <Row>{makeOpportunities()}</Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Dashboard;
