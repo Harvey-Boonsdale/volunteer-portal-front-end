59368a41b2fe3cc91ad021cd45ca5ff8f0f506d9
Harvey-Boonsdale <111384710+Harvey-Boonsdale@users.noreply.github.com>
2023-03-08 20:58:49 +0000

    landing page complete but needs work

---
+++ src/ViewOpportunitiesButton.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Button from "react-bootstrap/Button";
+
+function ViewOpportunities() {
+  return (
+    <Button size="lg" className="loginButton">
+      View Opportunities
+    </Button>
+  );
+}
+export default ViewOpportunities;
