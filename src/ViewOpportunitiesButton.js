c69e8e49349c7d76a9ebd2229c84a80a08000467
Graham Booth <Graham.Booth@sheffield.gov.uk>
2023-03-14 20:15:14 +0000

    button link fixed

--- src/ViewOpportunitiesButton.js
+++ src/ViewOpportunitiesButton.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 function ViewOpportunities() {
   return (
-    <Button size="lg" className="loginButton">
-      View Opportunities
-    </Button>
+    <div className="adminLoginButton">
+      <Link size="lg" className="link btn btn-primary loginButton" to="view">
+        View Opportunities
+      </Link>
+    </div>
   );
 }
 export default ViewOpportunities;
