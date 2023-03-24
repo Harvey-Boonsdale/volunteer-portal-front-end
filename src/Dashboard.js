90d9714926ad8024607abd2f34b0a34bb12c5c7a
Graham Booth <Graham.Booth@sheffield.gov.uk>
2023-03-24 14:41:59 +0000

    Admin cards added

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import "./App.css";
 import OpportunityCard from "./OpportunityCard";
+import AdminCard from "./AdminCard";
 
 function Dashboard(props) {
   useEffect(() => {
@@ -12,9 +13,18 @@ function Dashboard(props) {
 
   const makeOpportunities = () => {
     return props.opportunities.map((opportunity) => {
-      return (
-        <div className="cards landingPage">
+      console.log(opportunity, props.token);
+      return !props.token ? (
+        <div className="cards">
           <OpportunityCard
+            postToDisplay={opportunity}
+            client={props.client}
+            listEvents={props.listOpportunities}
+          />
+        </div>
+      ) : (
+        <div>
+          <AdminCard
             postToDisplay={opportunity}
             client={props.client}
             listEvents={props.listOpportunities}
@@ -29,7 +39,7 @@ function Dashboard(props) {
   return (
     <div>
       <Container>
-        <Row>{makeOpportunities()}</Row>
+        <Row className="cards">{makeOpportunities()}</Row>
       </Container>
     </div>
   );
