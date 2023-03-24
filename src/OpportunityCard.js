a2ebf6af0f6260ff96c17e9c7b82548f71559320
Graham Booth <Graham.Booth@sheffield.gov.uk>
2023-03-24 10:55:03 +0000

    Button added to card

--- src/OpportunityCard.js
+++ src/OpportunityCard.js
@@ -1,22 +1,42 @@
 import Card from "react-bootstrap/Card";
-// import Button from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 import "./App.css";
-// import { Link } from "react-router-dom";
 
 function OpportunityCard(props) {
+  //function to register for
+
+  const registrationHandler = () => {
+    alert("Registration Complete");
+  };
+
   return (
     <div className="opportunityCard">
       <Card style={{ width: "18rem" }}>
         <Card.Body>
-          <Card.Title>{props.postToDisplay.name}</Card.Title>
-          <Card.Text>School: {props.postToDisplay.school} </Card.Text>
-          <Card.Text>Details: {props.postToDisplay.details}</Card.Text>
-          <Card.Text>Time: {props.postToDisplay.time}</Card.Text>
-          <Card.Text>Date: {props.postToDisplay.date}</Card.Text>
+          <Card.Title>
+            <strong>{props.postToDisplay.name}</strong>
+          </Card.Title>
           <Card.Text>
-            Time Commitment: {props.postToDisplay.commitment}
+            <strong>School:</strong> {props.postToDisplay.school}{" "}
           </Card.Text>
-          <Card.Text>Location: {props.postToDisplay.location}</Card.Text>
+          <Card.Text>
+            <strong>Details:</strong> {props.postToDisplay.details}
+          </Card.Text>
+          <Card.Text>
+            <strong>Time:</strong> {props.postToDisplay.time}
+          </Card.Text>
+          <Card.Text>
+            <strong>Date:</strong> {props.postToDisplay.date}
+          </Card.Text>
+          <Card.Text>
+            <strong>Time Commitment:</strong> {props.postToDisplay.commitment}
+          </Card.Text>
+          <Card.Text>
+            <strong>Location:</strong> {props.postToDisplay.location}
+          </Card.Text>
+          <Button className="loginButton" onClick={() => registrationHandler()}>
+            Register for Opportunity
+          </Button>
         </Card.Body>
       </Card>
     </div>
