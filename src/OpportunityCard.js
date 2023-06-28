2a32a94bbb345311efb2c4c5ddc5b120be206ba4
Graham Booth <Graham.Booth@sheffield.gov.uk>
2023-06-28 19:23:59 +0100

    cards fixed

--- src/OpportunityCard.js
+++ src/OpportunityCard.js
@@ -1,35 +1,45 @@
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 import "./App.css";
+import { DateTime } from "luxon";
 
 function OpportunityCard(props) {
   //function to register for Opportunity
 
+  const isoString = props.postToDisplay.date;
+
+  const formatted = DateTime.fromISO(isoString)
+    .setZone("UTC")
+    .toLocaleString(DateTime.DATE_FULL);
+
+  console.log(formatted);
+
   return (
     <div>
-      <Card style={{ width: "27rem" }}>
+      <Card style={{ width: "20rem", height: "32rem " }}>
         <Card.Body>
           <Card.Title>
-            <strong>{props.postToDisplay.name}</strong>
+            <strong>
+              {props.postToDisplay.name} at {props.postToDisplay.school}
+            </strong>
           </Card.Title>
-          <Card.Text>{props.postToDisplay.school} </Card.Text>
+
           <Card.Text>
             <strong>Details:</strong> {props.postToDisplay.details}
           </Card.Text>
           <Card.Text>
-            <strong>Year Group:</strong> {props.postToDisplay.year}
+            <strong>Date:</strong> {formatted}
           </Card.Text>
           <Card.Text>
-            <strong>Start Time:</strong> {props.postToDisplay.startTime}
+            <strong>Start:</strong> {props.postToDisplay.startTime}{" "}
+            <strong>Finish:</strong> {props.postToDisplay.finishTime}
           </Card.Text>
-          <Card.Text>
-            <strong>Finish Time:</strong> {props.postToDisplay.finishTime}
-          </Card.Text>
-          <Card.Text>
-            <strong>Date:</strong> {props.postToDisplay.date}
-          </Card.Text>
+
           <Card.Text>
             <strong>Time Commitment:</strong> {props.postToDisplay.commitment}
+          </Card.Text>
+          <Card.Text>
+            <strong>Year Group:</strong> {props.postToDisplay.year}
           </Card.Text>
           <Card.Text>
             <strong>Location:</strong> {props.postToDisplay.location}
